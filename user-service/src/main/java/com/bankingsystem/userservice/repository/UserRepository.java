be89fce3a6161465b4df0e837893f87ca4e7eddc
Gaurav Kishore <151769095+igauravkishore@users.noreply.github.com>
2025-08-26 14:48:04 +0530

    commit

--- user-service/src/main/java/com/bankingsystem/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/bankingsystem/userservice/repository/UserRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByUsername(String username);
-
+    Optional<User> findByKeycloakId(String keycloakId);
     boolean existsByEmail(String email);
     boolean existsByPhoneNumber(String phoneNumber);
 }
